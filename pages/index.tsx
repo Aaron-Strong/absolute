ae4b109eaade4956809a8aa4647d96fdc5597da3
Aaron Strong <aaronspong1212@gmail.com>
2022-08-09 19:17:39 +0100

    Configure Debugging

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,17 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <h2
+          className={styles.description}
+          style={{
+            fontSize: '1.5rem',
+            fontWeight: 'normal',
+            lineHeight: '1.5',
+            marginTop: '1rem',
+          }}
+        >
+          Hello World!
+        </h2>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
